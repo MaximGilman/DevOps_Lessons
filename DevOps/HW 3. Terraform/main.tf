6e1470f38fd0254a57d76619bc7e48f74b0fa6ef
GilmanMaxim <maxim.gilman@yandex.ru>
2020-12-27 13:35:55 +0300

    починил работу БД

--- DevOps/HW 3. Terraform/main.tf
+++ DevOps/HW 3. Terraform/main.tf
@@ -1,20 +1,18 @@
-
 provider "aws" {
   access_key = ""
   secret_key = ""
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 
 resource "aws_eip" "static_ip" {
-  instance = aws_instance.My_Ubuntu.id
+  instance = aws_instance.my_ubuntu.id
 }
 
-resource "aws_instance" "My_Ubuntu" {
-  ami = "ami-0a91cd140a1fc148a (64-bit x86)" #Ubuntu linux
+resource "aws_instance" "my_ubuntu" {
+  ami = "ami-0e169fa5b2b2f88ae" #Ubuntu linux 64
   instance_type = "t3.micro"
   vpc_security_group_ids = [ aws_security_group.my_webserver.id ]
-  key_name = "my_precreated_ssh_key" #for ssh
   user_data = file("user_script.sh")
 
   tags = {
@@ -22,17 +20,7 @@ resource "aws_instance" "My_Ubuntu" {
     "Owner" = "Gilman Maxim"
   }
 }
-resource "aws_db_instance" "My_Db" {
-  name                 = "devops_task_db"
-  engine               = "postgres"
-  instance_class       = "db.t2.micro"
-  username             = "dbadmin"
-  password             = "password"
-  tags = {
-    "Name" = "DevOps AWS",
-    "Owner" = "Gilman Maxim"
-  }
-}
+
 
 resource "aws_security_group" "my_webserver" {
   name = "WS SG"
@@ -72,5 +60,55 @@ egress = [ {
   }
 }
 
+resource "aws_autoscaling_group" "load_bal_example1" {
 
+    min_size = 2
+    max_size = 4
+    launch_configuration = aws_launch_configuration.lb_instance1.id
 
+    load_balancers = [aws_elb.lb_example.name]
+    health_check_type = "ELB"
+    availability_zones = data.aws_availability_zones.any.names
+
+    tag {
+        key = "instanse_with_balancer_name1" 
+        value = "load_bal_example1"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_launch_configuration" "lb_instance1" {
+  image_id = "ami-0e169fa5b2b2f88ae" 
+  instance_type = "t3.micro"
+  user_data = file("user_script.sh")
+  security_groups = [ aws_security_group.my_webserver.id ]  
+}
+resource "aws_elb" "lb_example" {
+    name = "mylbexample"
+    availability_zones = data.aws_availability_zones.any.names
+    security_groups = [aws_security_group.my_webserver.id]
+
+    health_check{
+        target = "HTTP:${var.server_port}/"
+        interval = 60
+        timeout = 5
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = var.server_port
+        instance_protocol = "http"
+    }
+}
+
+variable "server_port" {
+    description = "HTTP"
+    type = number
+    default = 8080
+}
+data "aws_availability_zones" "any" {
+  
+}
--- DevOps/HW 3. Terraform/outputs.tf
+++ DevOps/HW 3. Terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "web_server_instance_id" {
-  value =  aws_instance.My_Ubuntu.id
+  value =  aws_instance.my_ubuntu.id
 }
 output "web_public_ip" {
   value =  aws_eip.static_ip.public_ip
